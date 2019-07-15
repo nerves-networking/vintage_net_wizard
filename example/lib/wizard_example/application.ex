2e071edb93e9773fcd07eec9bb2d9317fca06d14
Frank Hunleth <fhunleth@troodon-software.com>
2019-07-15 16:39:36 -0400

    Elixir 1.9/Nerves 1.5.0 support

--- example/lib/wizard_example/application.ex
+++ example/lib/wizard_example/application.ex
@@ -1,8 +1,8 @@
-defmodule WizardExampleFw.Application do
+defmodule WizardExample.Application do
   use Application
 
   def start(_type, _args) do
-    opts = [strategy: :one_for_one, name: WizardExampleFw.Supervisor]
+    opts = [strategy: :one_for_one, name: WizardExample.Supervisor]
 
     Supervisor.start_link([], opts)
   end
