cc358dfd92cdbdd4a0a47f501bc5ed1da6f3e698
Frank Hunleth <fhunleth@troodon-software.com>
2019-07-15 16:39:36 -0400

    Elixir 1.9/Nerves 1.5.0 support

--- example/mix.exs
+++ example/mix.exs
@@ -1,18 +1,23 @@
 defmodule WizardExample.MixProject do
   use Mix.Project
 
+  @app :wizard_example
+  @version "0.1.0"
+
   @all_targets [:rpi0, :rpi3, :rpi3a]
 
   def project do
     [
-      app: :wizard_example,
-      version: "0.1.0",
-      elixir: "~> 1.8",
-      archives: [nerves_bootstrap: "~> 1.4"],
+      app: @app,
+      version: @version,
+      elixir: "~> 1.9",
+      archives: [nerves_bootstrap: "~> 1.6"],
       start_permanent: Mix.env() == :prod,
       build_embedded: false,
       aliases: [loadconfig: [&bootstrap/1]],
-      deps: deps()
+      deps: deps(),
+      releases: [{@app, release()}],
+      preferred_cli_target: [run: :host, test: :host]
     ]
   end
 
@@ -35,8 +40,8 @@ defmodule WizardExample.MixProject do
   defp deps do
     [
       # Dependencies for all targets
-      {:nerves, "~> 1.4", runtime: false},
-      {:shoehorn, "~> 0.4"},
+      {:nerves, "~> 1.5.0", runtime: false},
+      {:shoehorn, "~> 0.6"},
       {:ring_logger, "~> 0.6"},
       {:toolshed, "~> 0.2"},
 
@@ -45,7 +50,17 @@ defmodule WizardExample.MixProject do
       {:busybox, "~> 0.1", targets: @all_targets},
       {:vintage_net_wizard, path: "../", targets: @all_targets},
       {:vintage_net, "~> 0.3", targets: @all_targets},
-      {:nerves_system_rpi0, "~> 1.8", targets: :rpi0}
+      {:nerves_system_rpi0, "~> 1.8", targets: :rpi0},
+      {:nerves_system_rpi3a, "~> 1.8", targets: :rpi3a}
+    ]
+  end
+
+  def release do
+    [
+      overwrite: true,
+      cookie: "#{@app}_cookie",
+      include_erts: &Nerves.Release.erts/0,
+      steps: [&Nerves.Release.init/1, :assemble]
     ]
   end
 end
