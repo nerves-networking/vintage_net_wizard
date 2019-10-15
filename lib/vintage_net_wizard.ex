351975d680486e98354891cfdd2aaa94d0887e07
Matt Ludwigs <mattludwigs@gmail.com>
2019-10-15 14:35:52 -0700

    Move SSL Configurations to be Options
    Moved the SSL configurations to be options that get passed into the
    `VintageNetWizard.run_wizard/1` function.
    
    The reason for this is to allow more flexible configuration appoarch
    for users of the library.
    
    More over removed the self signed certs as those are not used anymore.

--- lib/vintage_net_wizard.ex
+++ lib/vintage_net_wizard.ex
@@ -9,18 +9,26 @@ defmodule VintageNetWizard do
   Run the wizard.
 
   This means the WiFi module will be put into access point
-  mode and the web server will be started
+  mode and the web server will be started.
+
+  Options:
+
+    - `:ssl` - A Keyword list of `:ssl.tls_server_options`
+
+  See `Plug.SSL.configure/1` for more information about the
+  SSL options.
   """
-  @spec run_wizard() :: :ok
-  def run_wizard() do
+  @spec run_wizard([Endpoint.opt()]) :: :ok | {:error, String.t()}
+  def run_wizard(opts \\ []) do
     with :ok <- Backend.reset(),
          :ok <- into_ap_mode(),
-         {:ok, _server} <- start_server(),
+         {:ok, _server} <- Endpoint.start_server(opts),
          :ok <- Backend.start_scan() do
       :ok
     else
       # Already running is still ok
       {:error, :already_started} -> :ok
+      error -> error
     end
   end
 
