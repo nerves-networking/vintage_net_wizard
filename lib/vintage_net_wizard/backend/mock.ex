c93ccea7a1b06d895d4475aa1fdd800ee1442738
Frank Hunleth <fhunleth@troodon-software.com>
2019-10-20 13:46:28 -0400

    Add URL to go to for debugging

--- lib/vintage_net_wizard/backend/mock.ex
+++ lib/vintage_net_wizard/backend/mock.ex
@@ -10,8 +10,12 @@ defmodule VintageNetWizard.Backend.Mock do
   alias VintageNetWizard.Web.Endpoint
   alias VintageNet.WiFi.AccessPoint
 
+  require Logger
+
   @impl true
   def init() do
+    _ = Logger.info("Go to http://localhost:#{Application.get_env(:vintage_net_wizard, :port)}/")
+
     # It either starts, already running, or will error. So
     # don't need to care about the return
     _ = Endpoint.start_server()
