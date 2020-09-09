2a5a9e52a676abb5db3d553e62c63f45c48a2c73
Frank Hunleth <fhunleth@troodon-software.com>
2020-09-09 15:31:38 -0400

    Fix mix test when non-root
    When captive portal is enabled, it tries to listen on port 80 so that it
    can issue redirects. This doesn't work unless you're running as root.

--- config/test.exs
+++ config/test.exs
@@ -1,7 +1,9 @@
 use Mix.Config
 
 config :vintage_net_wizard,
-  backend: VintageNetWizard.Test.Backend
+  backend: VintageNetWizard.Test.Backend,
+  port: 4001,
+  captive_portal: false
 
 config :vintage_net,
   path: "#{File.cwd!()}/test/fixtures/root/bin"
