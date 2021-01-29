fda4ec0d4eab163a9f6b95a11e59ca0093bb0908
Frank Hunleth <fhunleth@troodon-software.com>
2021-01-29 18:37:50 -0500

    Refresh example app

--- example/config/target.exs
+++ example/config/target.exs
@@ -1,8 +1,32 @@
-use Mix.Config
+import Config
 
-# Authorize the device to receive firmware using your public key.
-# See https://hexdocs.pm/nerves_firmware_ssh/readme.html for more information
-# on configuring nerves_firmware_ssh.
+# Use shoehorn to start the main application. See the shoehorn
+# docs for separating out critical OTP applications such as those
+# involved with firmware updates.
+
+config :shoehorn,
+  init: [:nerves_runtime, :nerves_pack],
+  app: Mix.Project.config()[:app]
+
+# Nerves Runtime can enumerate hardware devices and send notifications via
+# SystemRegistry. This slows down startup and not many programs make use of
+# this feature.
+
+config :nerves_runtime, :kernel, use_system_registry: false
+
+# Erlinit can be configured without a rootfs_overlay. See
+# https://github.com/nerves-project/erlinit/ for more information on
+# configuring erlinit.
+
+config :nerves,
+  erlinit: [
+    hostname_pattern: "nerves-%s"
+  ]
+
+# Configure the device for SSH IEx prompt access and firmware updates
+#
+# * See https://hexdocs.pm/nerves_ssh/readme.html for general SSH configuration
+# * See https://hexdocs.pm/ssh_subsystem_fwup/readme.html for firmware updates
 
 keys =
   [
@@ -20,13 +44,48 @@ if keys == [],
     See your project's config.exs for this error message.
     """)
 
-config :nerves_firmware_ssh,
+config :nerves_ssh,
   authorized_keys: Enum.map(keys, &File.read!/1)
 
+# Configure the network using vintage_net
+# See https://github.com/nerves-networking/vintage_net for more information
 config :vintage_net,
   regulatory_domain: "US",
   config: [
+    {"usb0", %{type: VintageNetDirect}},
     {"wlan0", %{type: VintageNetWiFi}}
+  ]
+
+config :mdns_lite,
+  # The `host` key specifies what hostnames mdns_lite advertises.  `:hostname`
+  # advertises the device's hostname.local. For the official Nerves systems, this
+  # is "nerves-<4 digit serial#>.local".  mdns_lite also advertises
+  # "nerves.local" for convenience. If more than one Nerves device is on the
+  # network, delete "nerves" from the list.
+
+  host: [:hostname, "nerves"],
+  ttl: 120,
+
+  # Advertise the following services over mDNS.
+  services: [
+    %{
+      name: "SSH Remote Login Protocol",
+      protocol: "ssh",
+      transport: "tcp",
+      port: 22
+    },
+    %{
+      name: "Secure File Transfer Protocol over SSH",
+      protocol: "sftp-ssh",
+      transport: "tcp",
+      port: 22
+    },
+    %{
+      name: "Erlang Port Mapper Daemon",
+      protocol: "epmd",
+      transport: "tcp",
+      port: 4369
+    }
   ]
 
 # Import target specific config. This must remain at the bottom
--- example/lib/wizard_example/application.ex
+++ example/lib/wizard_example/application.ex
@@ -3,11 +3,6 @@ defmodule WizardExample.Application do
 
   @spec start(Application.start_type(), any()) :: {:error, any} | {:ok, pid()}
   def start(_type, _args) do
-    # Start the ssh server. In a real application, we'd need to link to the
-    # daemon pid that's returned and put it in a supervision tree so that
-    # crashes get handled properly.
-    _ = start_ssh()
-
     opts = [strategy: :one_for_one, name: WizardExample.Supervisor]
 
     gpio_pin = Application.get_env(:wizard_example, :gpio_pin, 17)
@@ -17,52 +12,5 @@ defmodule WizardExample.Application do
     ]
 
     Supervisor.start_link(children, opts)
-  end
-
-  def start_ssh() do
-    # Reuse keys from `nerves_firmware_ssh` so that the user only needs one
-    # config.exs entry.
-    authorized_keys =
-      Application.get_env(:nerves_firmware_ssh, :authorized_keys, [])
-      |> Enum.join("\n")
-
-    decoded_authorized_keys = :public_key.ssh_decode(authorized_keys, :auth_keys)
-
-    cb_opts = [authorized_keys: decoded_authorized_keys]
-
-    # Nerves stores a system default iex.exs. It's not in IEx's search path,
-    # so run a search with it included.
-    iex_opts = [dot_iex_path: find_iex_exs()]
-
-    # Reuse the system_dir as well to allow for auth to work with the shared
-    # keys.
-    :ssh.daemon(22, [
-      {:id_string, :random},
-      {:key_cb, {Nerves.Firmware.SSH.Keys, cb_opts}},
-      {:system_dir, Nerves.Firmware.SSH.Application.system_dir()},
-      {:shell, {Elixir.IEx, :start, [iex_opts]}},
-      {:exec, &start_exec/3},
-      {:subsystems, [:ssh_sftpd.subsystem_spec(cwd: '/')]}
-    ])
-  end
-
-  defp find_iex_exs() do
-    [".iex.exs", "~/.iex.exs", "/etc/iex.exs"]
-    |> Enum.map(&Path.expand/1)
-    |> Enum.find("", &File.regular?/1)
-  end
-
-  defp start_exec(cmd, user, peer) do
-    spawn(fn -> exec(cmd, user, peer) end)
-  end
-
-  defp exec(cmd, _user, _peer) do
-    try do
-      {result, _env} = Code.eval_string(to_string(cmd))
-      IO.inspect(result)
-    catch
-      kind, value ->
-        IO.puts("** (#{kind}) #{inspect(value)}")
-    end
   end
 end
