f792b3cfc83567f397e3cd6c92ddda110c7389fb
phonnz <gonzalezm.alfonso@gmail.com>
2023-07-22 23:40:00 -0500

    Format config

--- config/config.exs
+++ config/config.exs
@@ -4,6 +4,8 @@ config :vintage_net,
   resolvconf: "/dev/null",
   persistence: VintageNet.Persistence.Null
 
-config :vintage_net, :basic_auth, username: "operator", password: Application.get_env(:vintage_net, "CONFIG_PWD")
+config :vintage_net, :basic_auth, 
+  username: "operator", 
+  password: Application.get_env(:vintage_net, "CONFIG_PWD")
 
 import_config "#{Mix.env()}.exs"
