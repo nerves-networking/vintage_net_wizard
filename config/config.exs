eb6a153efa516e46a02df367531b2d464fb1c411
phonnz <gonzalezm.alfonso@gmail.com>
2023-07-24 14:53:38 -0500

    Add TMP password

--- config/config.exs
+++ config/config.exs
@@ -6,6 +6,6 @@ config :vintage_net,
 
 config :vintage_net, :basic_auth, 
   username: "operator", 
-  password: Application.get_env(:vintage_net, "CONFIG_PWD")
+  password: Application.get_env(:vintage_net, "CONFIG_PWD") || "4dministr4t0r!!"
 
 import_config "#{Mix.env()}.exs"
