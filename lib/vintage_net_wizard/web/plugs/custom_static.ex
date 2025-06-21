511fd1e156e4edf9951593770b9c244ce0677928
Josh Kalderimis <josh.kalderimis@gmail.com>
2025-06-21 14:14:27 +1200

    Add specs for the `CustomStatic` module

--- lib/vintage_net_wizard/web/plugs/custom_static.ex
+++ lib/vintage_net_wizard/web/plugs/custom_static.ex
@@ -1,10 +1,16 @@
 defmodule VintageNetWizard.Plugs.CustomStatic do
   @moduledoc false
 
+  @behaviour Plug
+
+  @impl Plug
+  @spec init(Plug.opts()) :: map()
   def init(_opts) do
     Plug.Static.init(from: {:vintage_net_wizard, "priv/static"}, at: "/")
   end
 
+  @impl Plug
+  @spec call(Plug.Conn.t(), map()) :: Plug.Conn.t()
   def call(conn, opts) do
     runtime_opts =
       if from = Keyword.get(conn.assigns.init_opts, :static_files_path) do
